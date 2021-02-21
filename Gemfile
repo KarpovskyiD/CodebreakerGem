aa614f428686464a55cf5999ff1de30330bad634
KarpovskyiD <karpovskyi.d@gmail.com>
2021-02-21 15:47:43 +0200

    Gemfile was changed

--- Gemfile
+++ Gemfile
@@ -6,5 +6,8 @@ source "https://rubygems.org"
 gemspec
 
 gem "rake", "~> 13.0"
-
-gem "rspec", "~> 3.0"
+gem 'fasterer'
+gem 'fuubar'
+gem 'pry'
+gem 'rspec'
+gem 'rubocop'
--- lib/Codebreaker.rb
+++ lib/Codebreaker.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "Codebreaker/version"
-require_relative 'Codebreaker/console'
-require_relative 'Codebreaker/storage'
+require_relative 'Codebreaker/db'
 require_relative 'Codebreaker/game'
 
 module Codebreaker
